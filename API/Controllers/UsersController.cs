b6a87e9cb98f6dd441e551299af4e8fb5de059f2
Pranjal Agrawal <pranjalagrawal3097@gmail.com>
2023-04-03 15:18:44 +0530

    End of Section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -17,7 +17,7 @@ namespace API.Controllers
 
         public UsersController(DataContext  context)
         {
-            _context = context;
+             _context = context;
         }
 
         [HttpGet]
